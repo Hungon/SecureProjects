ee7dd91421b4c7b6e4a0a9b4a19f7abe9097790d
kohei.moroi <kohei.moroi@softbankrobotics.com>
2019-03-28 18:10:24 +0900

    dialog to login shows up

--- LoginApp/app/src/main/java/com/trials/loginapp/Authenticator.kt
+++ LoginApp/app/src/main/java/com/trials/loginapp/Authenticator.kt
@@ -3,21 +3,35 @@ package com.trials.loginapp
 import android.accounts.AbstractAccountAuthenticator
 import android.accounts.Account
 import android.accounts.AccountAuthenticatorResponse
+import android.accounts.AccountManager
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 
 class Authenticator(private val context: Context) : AbstractAccountAuthenticator(context) {
 
 
-    override fun getAuthTokenLabel(authTokenType: String?): String {
+    private fun accountExist(account: Account): Boolean {
+        val am = AccountManager.get(context)
+        val accounts = am.getAccountsByType(TRIALS_ACCOUNT_TYPE)
+        for (ac in accounts) {
+            if (ac == account) {
+                return true
+            }
+        }
+        return false
+    }
 
+    override fun getAuthTokenLabel(authTokenType: String?): String {
+        return TRIALS_AUTHTOKEN_LABEL
     }
 
     override fun confirmCredentials(
         response: AccountAuthenticatorResponse?,
         account: Account?,
         options: Bundle?
-    ): Bundle {
+    ): Bundle? {
+        return null
     }
 
     override fun updateCredentials(
@@ -25,7 +39,8 @@ class Authenticator(private val context: Context) : AbstractAccountAuthenticator
         account: Account?,
         authTokenType: String?,
         options: Bundle?
-    ): Bundle {
+    ): Bundle? {
+        return null
     }
 
     override fun getAuthToken(
@@ -34,6 +49,17 @@ class Authenticator(private val context: Context) : AbstractAccountAuthenticator
         authTokenType: String?,
         options: Bundle?
     ): Bundle {
+        val bundle = Bundle();
+        if (accountExist(account)) {
+            val intent = Intent(context, LoginActivity::class.java)
+            intent.putExtra(RE_AUTH_NAME, account?.name)
+            intent.putExtra(AccountManager.KEY_ACCOUNT_AUTHENTICATOR_RESPONSE, response)
+            bundle.putParcelable(AccountManager.KEY_INTENT, intent)
+        } else {
+            bundle.putString(AccountManager.KEY_ERROR_CODE, String.valueOf(-2))
+            bundle.putString(AccountManager.KEY_ERROR_MESSAGE, context.getString(R.string.error_account_not_exists));
+        }
+        return bundle
     }
 
     override fun hasFeatures(
@@ -41,9 +67,13 @@ class Authenticator(private val context: Context) : AbstractAccountAuthenticator
         account: Account?,
         features: Array<out String>?
     ): Bundle {
+        val result = Bundle()
+        result.putBoolean(AccountManager.KEY_BOOLEAN_RESULT, false)
+        return result
     }
 
-    override fun editProperties(response: AccountAuthenticatorResponse?, accountType: String?): Bundle {
+    override fun editProperties(response: AccountAuthenticatorResponse?, accountType: String?): Bundle? {
+        return null
     }
 
     override fun addAccount(
@@ -53,12 +83,23 @@ class Authenticator(private val context: Context) : AbstractAccountAuthenticator
         requiredFeatures: Array<out String>?,
         options: Bundle?
     ): Bundle {
+        val am = AccountManager.get(context)
+        val accounts = am.getAccountsByType(TRIALS_ACCOUNT_TYPE);
+        val bundle = Bundle()
+        if (accounts.isNotEmpty()) {
+            context.getString(R.string.error_account_exists))
+        } else {
+            val intent = Intent(context, LoginActivity::class.java)
+            intent.putExtra(AccountManager.KEY_ACCOUNT_AUTHENTICATOR_RESPONSE, response)
+            bundle.putParcelable(AccountManager.KEY_INTENT, intent)
+        }
+        return bundle
     }
 
     companion object {
-        val JSSEC_ACCOUNT_TYPE = "org.jssec.android.accountmanager"
-        val JSSEC_AUTHTOKEN_TYPE = "webservice"
-        val JSSEC_AUTHTOKEN_LABEL = "JSSEC Web Service"
+        val TRIALS_ACCOUNT_TYPE = "com.trials.loginapp.accountmanager"
+        val TRIALS_AUTHTOKEN_TYPE = "webservice"
+        val TRIALS_AUTHTOKEN_LABEL = "Trials Web Service"
         val RE_AUTH_NAME = "reauth_name"
     }
 }
--- LoginApp/app/src/main/java/com/trials/loginapp/LoginActivity.kt
+++ LoginApp/app/src/main/java/com/trials/loginapp/LoginActivity.kt
@@ -2,11 +2,90 @@ package com.trials.loginapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.accounts.AccountAuthenticatorActivity
+import android.os.Build
+import android.text.InputType
+import android.view.Window
+import android.accounts.AccountManager
+import android.support.v4.app.NotificationCompat.getExtras
+import android.content.Intent
+import android.accounts.Account
+import android.app.Activity
+import android.text.TextUtils
+import android.util.Log
+import android.view.View
+import sun.security.jgss.GSSUtil.login
 
-class LoginActivity : AppCompatActivity() {
+
+
+
+class LoginActivity : AccountAuthenticatorActivity() {
+
+    private var reAuthName: String? = null
+    private var nameEdit: EditText? = null
+    private var passEdit: EditText? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_activity)
+
+        requestWindowFeature(Window.FEATURE_LEFT_ICON)
+        setContentView(R.layout.login_activity)
+        window.setFeatureDrawableResource(Window.FEATURE_LEFT_ICON, android.R.drawable.ic_dialog_alert)
+        nameEdit = findViewById (R.id.username_edit)
+        passEdit = findViewById (R.id.password_edit)
+        reAuthName = intent.getStringExtra(Authenticator.RE_AUTH_NAME)
+        if (reAuthName != null) {
+            nameEdit?.setText(reAuthName)
+            nameEdit?.inputType = InputType.TYPE_NULL
+            nameEdit?.isFocusable = false
+            nameEdit?.isEnabled = false
+        }
+    }
+
+    fun handleLogin(view: View) {
+        val name = nameEdit?.text.toString()
+        val pass = passEdit?.text.toString()
+        if (TextUtils.isEmpty(name) || TextUtils.isEmpty(pass)) {
+            setResult(RESULT_CANCELED)
+            finish()
+        }
+        val web = WebService()
+        val authToken = web.login(name, pass)
+        if (TextUtils.isEmpty(authToken)) {
+            setResult(RESULT_CANCELED)
+            finish()
+        }
+        Log.i(TAG, "WebService login succeeded")
+        if (reAuthName == null) {
+            val am = AccountManager.get(this)
+            val account = Account(name, Authenticator.TRIALS_ACCOUNT_TYPE)
+            am.addAccountExplicitly(account, null, null)
+            am.setAuthToken(account, Authenticator.TRIALS_AUTHTOKEN_TYPE, authToken)
+            val intent = Intent()
+            intent.putExtra(AccountManager.KEY_ACCOUNT_NAME, name)
+            intent.putExtra(
+                AccountManager.KEY_ACCOUNT_TYPE,
+                Authenticator.TRIALS_ACCOUNT_TYPE
+            )
+            setAccountAuthenticatorResult(intent.extras)
+            setResult(Activity.RESULT_OK, intent)
+        } else {
+            val bundle = Bundle()
+            bundle.putString(AccountManager.KEY_ACCOUNT_NAME, name)
+            bundle.putString(
+                AccountManager.KEY_ACCOUNT_TYPE,
+                Authenticator.TRIALS_ACCOUNT_TYPE
+            )
+            bundle.putString(AccountManager.KEY_AUTHTOKEN, authToken)
+            setAccountAuthenticatorResult(bundle)
+            setResult(Activity.RESULT_OK)
+        }
+        finish()
+    }
+
+    companion object {
+        private val TAG = AccountAuthenticatorActivity::class.java.simpleName
     }
 }
