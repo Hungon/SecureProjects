3fffc6b74ef18ddc0a26243c572ee78b86b785bd
kohei.moroi <kohei.moroi@softbankrobotics.com>
2019-04-08 14:46:07 +0900

    first commit

---
+++ SecureImageLoader/app/src/main/java/com/trials/secureimageloader/HttpImageSearchTask.kt
@@ -0,0 +1,97 @@
+package com.trials.secureimageloader
+
+import android.os.AsyncTask
+import android.util.Log
+import org.json.JSONException
+import org.json.JSONObject
+import java.io.BufferedInputStream
+import java.io.ByteArrayOutputStream
+import java.io.IOException
+import java.net.HttpURLConnection
+import java.net.URL
+
+
+abstract class HttpImageSearchTask : AsyncTask<String, Void, Any?>() {
+
+    @Throws(IOException::class)
+    private fun checkResponse(response: HttpURLConnection) {
+        val statusCode = response.responseCode
+        if (HttpURLConnection.HTTP_OK != statusCode) {
+            throw IOException("HttpStatus: $statusCode")
+        }
+    }
+
+    private fun getByteArray(strUrl: String): ByteArray? {
+        val buff = ByteArray(1024)
+        var result: ByteArray? = null
+        val response: HttpURLConnection
+        var inputStream: BufferedInputStream? = null
+        var responseArray: ByteArrayOutputStream? = null
+        var length = 0
+        try {
+            val url = URL(strUrl)
+            response = url.openConnection() as HttpURLConnection
+            response.requestMethod = "GET"
+            response.connect()
+            checkResponse(response)
+            inputStream = BufferedInputStream(response.inputStream)
+            responseArray = ByteArrayOutputStream()
+            while (length != -1) {
+                length = inputStream.read(buff)
+                if (length > 0) responseArray.write(buff, 0, length)
+            }
+            result = responseArray.toByteArray()
+        } catch (e: IOException) {
+            Log.e(TAG, e.message)
+        } finally {
+            try {
+                inputStream?.close()
+            } catch (e: IOException) {
+                try {
+                    responseArray?.close()
+                } catch (e: IOException) {
+                    Log.e(TAG, e.message)
+                }
+            }
+        }
+        return result
+    }
+
+    public override  fun doInBackground(vararg params: String?): Any? {
+        var responseArray: ByteArray?
+        // Search image
+        // not include sensitive info
+        val s = StringBuilder()
+        for (param in params) {
+            s.append(param)
+            s.append('+')
+        }
+        s.deleteCharAt(s.length - 1)
+        val search_url = "http://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=" + s.toString()
+        responseArray = getByteArray(search_url)
+        if (responseArray == null) {
+            return null
+        }
+        val image_url: String
+        try {
+            val json = String(responseArray)
+            image_url = JSONObject(json).getJSONObject("responseData")
+                .getJSONArray("results").getJSONObject(0).getString("url")
+        } catch (e: JSONException) {
+            return e
+        }
+        // Get image
+        if (image_url != null) {
+            responseArray = getByteArray(image_url)
+            if (responseArray == null) {
+                return null
+            }
+        }
+        return responseArray
+    }
+
+    companion object {
+        private val TAG = HttpImageSearchTask::class.java.simpleName
+    }
+
+}
---
+++ SecureImageLoader/app/src/main/java/com/trials/secureimageloader/MainActivity.kt
@@ -0,0 +1,92 @@
+package com.trials.secureimageloader
+
+import android.graphics.BitmapFactory
+import android.os.AsyncTask
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.EditText
+import android.widget.ImageView
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var queryBox: EditText
+    private lateinit var searchButtonWithHttp: Button
+    private lateinit var searchButtonWithHttps: Button
+    private lateinit var msgBox: TextView
+    private lateinit var imgBox: ImageView
+    private var asyncTask: AsyncTask<String, Void, Any?>? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        queryBox = findViewById(R.id.edit_query)
+        searchButtonWithHttp = findViewById(R.id.button_search_with_http)
+        searchButtonWithHttps = findViewById(R.id.button_search_with_https)
+        msgBox = findViewById(R.id.text_message)
+        imgBox = findViewById(R.id.image_result)
+
+        searchButtonWithHttp.setOnClickListener {
+            onHttpSearchClick()
+        }
+
+        searchButtonWithHttps.setOnClickListener {
+            onHttpsSearchClick()
+        }
+    }
+
+    override fun onPause() {
+        asyncTask?.cancel(true)
+        super.onPause()
+    }
+
+    private fun onHttpSearchClick() {
+        val query = queryBox.text.toString()
+        msgBox.text = String.format("HTTP:%s", query)
+        imgBox.setImageBitmap(null)
+        asyncTask?.cancel(true)
+        asyncTask = object : HttpImageSearchTask() {
+
+            override fun onPostExecute(result: Any?) {
+                when (result) {
+                    null -> {
+                        msgBox.append("\nException happened\n")
+                    }
+                    is Exception -> {
+                        msgBox.append("\nException happened\n  $result")
+                    }
+                    else -> {
+                        val data = result as ByteArray
+                        val bmp = BitmapFactory.decodeByteArray(data, 0, data.size)
+                        imgBox.setImageBitmap(bmp)
+                    }
+
+                }
+            }
+        }.execute(query)
+    }
+
+    private fun onHttpsSearchClick() {
+        val query = queryBox.text.toString()
+        msgBox.text = String.format("HTTPS:%s", query)
+        imgBox.setImageBitmap(null)
+        asyncTask?.cancel(true)
+        asyncTask = object : HttpsImageSearchTask() {
+            override fun onPostExecute(result: Any?) {
+                if (result is Exception) {
+                    msgBox.append("\nException happened\n$result")
+                } else {
+                    val data = result as ByteArray
+                    val bmp = BitmapFactory.decodeByteArray(data, 0, data.size)
+                    imgBox.setImageBitmap(bmp)
+                }
+            }
+        }.execute(query)
+    }
+
+    companion object {
+        private val TAG = MainActivity::class.java.simpleName
+    }
+}
